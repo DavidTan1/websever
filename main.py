526b09aa9ec13626f87179f1233d50ec6659440d
DavidTan1 <dtan2785@bths.edu>
2021-03-10 20:11:28 -0500

    get request now works with query strings

--- main.py
+++ main.py
@@ -2,30 +2,62 @@ import socketserver
 import codecs
 
 
+def setUpTemplate(map, html):
+    if "{{loop}}" in html:
+        newhtml = html.replace("{{loop}}", "arandomStringUseToSPLIT{{loop}}")
+        updatehtml = newhtml.replace("{{end_loop}}", "{{end_loop}}arandomStringUseToSPLIT")
+        htmlarr = updatehtml.split("arandomStringUseToSPLIT")
+        for x in htmlarr:
+            if "{{loop}}" in x:
+                x1 = x.replace("{{loop}}", "")
+                x2 = x1.replace("{{end_loop}}", "")
+                x3 = x2.replace("{{", "")
+                x4 = x3.replace("}}", "")
+
+                for y in map.keys():
+                    if y in x4:
+                        arr = x4.split("=")
+                        imagearr = map[y].split('+')  # get all the content
+                        img = ""
+                        for z in imagearr:
+
+                            stry = str(y)
+                            newy = stry.replace('s','')
+                            img = img + arr[0] + '="/' + newy + '/' + z + '.jpg"'+"/>"+'\r\n'
+                            print(img)
+                        html = html.replace(x, img)
+
+    for key in map.keys():
+        html = html.replace("{{" + key + "}}", map[key])  # replace placeholder with key value
+    #print(html)
+    return html
+
+
 def requestFor200(pathname):
     string = "HTTP/1.1 200 OK\r\nContent-Type:"
     if pathname == "/hello":
         string = string + " text/plain\r\nContent-Length: 12\r\n\r\nHello World!"
         return string
+
     if pathname == "/":
         html = codecs.open("index.html", 'r')
         htmlcontent = html.read()
         count = 0
         for x in htmlcontent:
             print(x.encode())
-            count+=1
-        return "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: "+str(count)+"\r\nX-Content-Type-Options: nosniff\r\n\r\n"+htmlcontent
+            count += 1
+        return "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + str(
+            count) + "\r\nX-Content-Type-Options: nosniff\r\n\r\n" + htmlcontent
 
     if pathname == "/style.css":
-
         css = codecs.open("style.css", 'r')
         csscontent = css.read()
         count = 0
         for x in csscontent:
             print(x.encode())
-            count+=1
-
-        return "HTTP/1.1 200 OK\r\nContent-Type: text/css\r\nContent-Length: "+str(count)+"\r\nX-Content-Type-Options: nosniff\r\n\r\n"+csscontent
+            count += 1
+        return "HTTP/1.1 200 OK\r\nContent-Type: text/css\r\nContent-Length: " + str(
+            count) + "\r\nX-Content-Type-Options: nosniff\r\n\r\n" + csscontent
 
     if pathname == "/functions.js":
         js = codecs.open("functions.js", 'r')
@@ -34,7 +66,8 @@ def requestFor200(pathname):
         for x in jscontent:
             print(x.encode())
             count += 1
-        return "HTTP/1.1 200 OK\r\nContent-Type: text/javascript\r\nContent-Length: "+str(count)+"\r\nX-Content-Type-Options: nosniff\r\n\r\n"+jscontent
+        return "HTTP/1.1 200 OK\r\nContent-Type: text/javascript\r\nContent-Length: " + str(
+            count) + "\r\nX-Content-Type-Options: nosniff\r\n\r\n" + jscontent
 
     if pathname == "/utf.txt":
         utf = codecs.open("utf.txt", 'r', encoding="utf-8")
@@ -42,12 +75,36 @@ def requestFor200(pathname):
         count = 0
         for x in utfcontent:
             btyearr = str(x.encode()).split("\\")
-
             for y in btyearr:
                 if y != "b'":
                     count += 1
         print(count)
-        return "HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=utf-8\r\nContent-Length: "+str(count)+"\r\nX-Content-Type-Options: nosniff\r\n\r\n"+utfcontent
+        return "HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=utf-8\r\nContent-Length: " + str(
+            count) + "\r\nX-Content-Type-Options: nosniff\r\n\r\n" + utfcontent
+
+    if "/image/" in pathname:
+        patharr = pathname.split('/')
+        imgcontent = open('image/' + patharr[2], 'rb').read()
+        count = 0
+        for x in imgcontent:
+            print(x)
+            count += 1
+        return ("HTTP/1.1 200 OK\r\nContent-Type: image/jpeg; charset=utf-8\r\nContent-Length: " + str(
+            count) + "\r\nX-Content-Type-Options: nosniff\r\n\r\n").encode() + imgcontent
+
+    if "/images" in pathname:
+        map = {}
+        patharr = pathname.split('?')
+        html = codecs.open("HTMLtemplate.html", 'r').read()
+        brokenupqueryString = patharr[1].split('&')
+        print(brokenupqueryString)
+        for x in brokenupqueryString:
+            keyandvalue = x.split('=')
+            map.update({keyandvalue[0]: keyandvalue[1]})
+
+        updatedhtml = setUpTemplate(map, html)
+        return "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: " + str(
+            len(updatedhtml)) + "\r\nX-Content-Type-Options: nosniff\r\n\r\n" + updatedhtml
 
 
 def requestFor404(pathname):
@@ -66,24 +123,32 @@ class tcp(socketserver.BaseRequestHandler):
         print(data.decode())
 
         parseData = data.decode().split('\r\n')
-        header = parseData[0]
-
+        header = parseData[0]  # the header
         headerLine = header.split(" ")
-        request = headerLine[0]
-        path = headerLine[1]
-        print(path)
-        print(headerLine)
-        if request == "GET" and path == "/hello":
+
+        request = headerLine[0]  # GET or POST request
+
+        path = headerLine[1]  # the path
+
+        paths = path.split("?")  # split the path to check for query string
+        # path is the first index
+        # the query string is the 2nd index
+
+        if request == "GET" and paths[0] == "/hello":
             self.request.send(requestFor200(path).encode())
-        elif request == "GET" and path == "/hi":
+        elif request == "GET" and paths[0] == "/hi":
             self.request.send(requestFor301(path).encode())
-        elif request == "GET" and path == "/":
+        elif request == "GET" and paths[0] == "/":
             self.request.send(requestFor200(path).encode())
-        elif request == "GET" and path == "/style.css":
+        elif request == "GET" and paths[0] == "/style.css":
             self.request.send(requestFor200(path).encode())
-        elif request == "GET" and path == "/functions.js":
+        elif request == "GET" and paths[0] == "/functions.js":
             self.request.send(requestFor200(path).encode())
-        elif request == "GET" and path == "/utf.txt":
+        elif request == "GET" and paths[0] == "/utf.txt":
+            self.request.send(requestFor200(path).encode())
+        elif request == "GET" and "/image/" in paths[0]:
+            self.request.send(requestFor200(path))
+        elif request == "GET" and "/images" == paths[0]:
             self.request.send(requestFor200(path).encode())
         else:
             self.request.send(requestFor404(path).encode())
--- test.py
+++ test.py
@@ -1,17 +1,5 @@
 if __name__ == '__main__':
 
-    n = 3
-    s = 1/n
-    string = str(s)
-    strings = string.split(".")
-    print(strings)
-    list = []
+    s = 'asfasfas[fashdljkflijoafijaolnsfsnf".split("[")'
 
-    for x in strings[1]:
-        list.append(x)
-        for y in list:
-            if(y = x):
-
-        print(x)
-
-
+    print(s + "/")
